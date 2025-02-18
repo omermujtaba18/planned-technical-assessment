5221c093cad5b188e60ed5eba3e51e874cb87bd5
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-02-18 02:23:09 -0500

    Add remote image configuration in next config

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+      },
+    ],
+  },
   /* config options here */
 };
 
