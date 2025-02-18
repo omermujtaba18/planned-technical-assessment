459535f4da530906f425e9943c1618a05df2803e
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-02-18 01:26:18 -0500

    Add CreateMemory component and integrate it into Home page

--- app/src/app/(platform)/page.tsx
+++ app/src/app/(platform)/page.tsx
@@ -1,7 +1,9 @@
+import CreateMemory from "@/components/home/create-memory";
+
 export default function Home() {
   return (
-    <div className="bg-gradient-to-b from-purple-50 to-white min-h-screen">
-      Home Page
-    </div>
+    <>
+      <CreateMemory />
+    </>
   );
 }
