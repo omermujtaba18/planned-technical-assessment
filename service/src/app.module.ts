8042bf1d6b8f6d827e3c7c5ebfdf0619aedbefe6
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-02-20 22:46:06 -0500

    Add Sequelize integration for PostgreSQL database connection

--- service/src/app.module.ts
+++ service/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration, {
+  IConfiguration,
+  IDatabaseConfiguration,
+} from './config/configuration';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService<IConfiguration>) => {
+        const dbConfig = configService.get<IDatabaseConfiguration>('database');
+        return {
+          dialect: 'postgres',
+          host: dbConfig.host,
+          port: +dbConfig.port,
+          username: dbConfig.username,
+          password: dbConfig.password,
+          database: dbConfig.database,
+          autoLoadModels: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
