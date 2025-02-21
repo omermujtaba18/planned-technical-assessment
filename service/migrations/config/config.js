f9f7fa837cf5cf7b264b1b9b4a9edbd8cb608d62
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-02-21 01:18:32 -0500

    Add Sequelize migration for Users table and configuration setup

---
+++ service/migrations/config/config.js
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres',
+  },
+};
