b3cd5e5651095661e4770fbcf5b226d23ed112fa
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-02-22 01:17:18 -0500

    Add Zustand for user state management and implement user fetching in Header

---
+++ app/src/store/userStore.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+interface User {
+  id: string;
+  fullName: string;
+  email: string;
+  profilePicture: string;
+}
+
+interface UserStore {
+  user: User | null;
+  setUser: (user: User) => void;
+  clearUser: () => void;
+}
+
+export const useUserStore = create<UserStore>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  clearUser: () => set({ user: null }),
+}));
