98ffc2520181bea90135d018c962785427663a51
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-02-22 03:13:14 -0500

    Refactor settings page to use ProfileSettingCard component and implement user profile update functionality

---
+++ app/src/components/settings/profile-settings-card.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { userProfileSchema } from "@/forms/schemas/user";
+import { useUserStore } from "@/store/userStore";
+import { Formik, Form } from "formik";
+import { ChangeEvent, useRef } from "react";
+import { ServerError } from "../shared/server-error";
+import { Button } from "../ui/button";
+import { Card } from "../ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { patchUserAction } from "@/forms/actions/user";
+
+export const ProfileSettingCard: React.FC = () => {
+  const { user } = useUserStore();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    setFieldValue: (
+      field: string,
+      value: unknown,
+      shouldValidate?: boolean,
+    ) => void,
+  ) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setFieldValue("profilePicture", event?.target?.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  if (!user) return null;
+
+  return (
+    <section id="profile-setting" className="mb-4">
+      <Card className="flex flex-col gap-4 shadow-none">
+        <h2 className="text-lg text-gray-900 font-bold">Profile Settings</h2>
+        <Formik
+          initialValues={user}
+          validationSchema={userProfileSchema.validationSchema}
+          onSubmit={patchUserAction}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            setFieldValue,
+            status,
+            handleSubmit,
+            handleChange,
+            isSubmitting,
+          }) => (
+            <Form onSubmit={handleSubmit} className="flex flex-col gap-8">
+              <div className="flex flex-col gap-5">
+                <div className="flex items-center gap-4">
+                  <Avatar className="size-16">
+                    <AvatarImage src={values.profilePicture} />
+                    <AvatarFallback>
+                      {user?.fullName
+                        .split(" ")
+                        .map((name) => name[0])
+                        .join("")
+                        .toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <Button
+                    variant="outline"
+                    onClick={() => fileInputRef.current?.click()}
+                    type="button"
+                  >
+                    Change Photo
+                  </Button>
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    accept="image/*"
+                    onChange={(e) => handleFileChange(e, setFieldValue)}
+                    hidden
+                  />
+                </div>
+                <div className="gap-2 max-w-sm flex flex-col">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    value={values.email}
+                    disabled
+                  />
+                </div>
+                <div className="gap-2 max-w-sm flex flex-col">
+                  <Label htmlFor="fullName">Full Name</Label>
+                  <Input
+                    type="text"
+                    id="fullName"
+                    defaultValue={user?.fullName}
+                    onChange={handleChange}
+                    placeholder="Enter your full name"
+                  />
+                  <p className="text-red-600 text-sm">
+                    {errors.fullName && touched.fullName && errors.fullName}
+                  </p>
+                </div>
+                <ServerError status={status} />
+              </div>
+
+              <Button
+                className="max-w-fit"
+                size="lg"
+                disabled={isSubmitting}
+                type="submit"
+              >
+                Save Changes
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Card>
+    </section>
+  );
+};
--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const base64ToFile = (
+  base64String: string,
+  fileName: string,
+  mimeType = "image/jpeg",
+) => {
+  const byteCharacters = atob(base64String.split(",")[1]);
+  const byteArrays = [];
+
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteArrays.push(byteCharacters.charCodeAt(i));
+  }
+
+  const byteArray = new Uint8Array(byteArrays);
+  return new File([byteArray], fileName, { type: mimeType });
+};
