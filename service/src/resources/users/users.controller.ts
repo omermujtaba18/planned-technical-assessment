00b0b02e3cfcfc324a415ed51be254cc24615486
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-02-22 03:16:49 -0500

    Add profile picture upload functionality and enforce full name length validation in user update

--- service/src/resources/users/users.controller.ts
+++ service/src/resources/users/users.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Body, Patch, Req, Get } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Patch,
+  Req,
+  Get,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import * as path from 'path';
 
 @Controller('users')
 export class UsersController {
@@ -12,7 +23,27 @@ export class UsersController {
   }
 
   @Patch('/me')
-  update(@Req() req, @Body() updateUserDto: UpdateUserDto) {
+  @UseInterceptors(
+    FileInterceptor('profilePicture', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+          const ext = path.extname(file.originalname);
+          const filename = `${file.fieldname}-${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  update(
+    @Req() req,
+    @UploadedFile() file?: Express.Multer.File,
+    @Body() updateUserDto?: UpdateUserDto,
+  ) {
+    if (file) {
+      updateUserDto.profilePicture = `http://localhost:5001/uploads/${file.filename}`;
+    }
     return this.usersService.update(req.user.id, updateUserDto);
   }
 }
