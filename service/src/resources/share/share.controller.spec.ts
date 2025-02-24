86d516e37240848c08b5bf5bc96ae4cd0d3bbb58
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-02-24 07:15:35 -0500

    Fix Share module tests

--- service/src/resources/share/share.controller.spec.ts
+++ service/src/resources/share/share.controller.spec.ts
@@ -4,17 +4,53 @@ import { ShareService } from './share.service';
 
 describe('ShareController', () => {
   let controller: ShareController;
+  let service: ShareService;
+
+  const mockShareService = {
+    findOne: jest.fn().mockImplementation((id) => ({
+      id: 1,
+      name: 'John Doe',
+      memories: [
+        {
+          id: 1,
+          title: 'Some title',
+          description: 'Some description',
+          media: [{ id: 1, url: 'http://example.com/image.jpg' }],
+        },
+      ],
+    })),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ShareController],
-      providers: [ShareService],
+      providers: [{ provide: ShareService, useValue: mockShareService }],
     }).compile();
 
     controller = module.get<ShareController>(ShareController);
+    service = module.get<ShareService>(ShareService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call findOne and return the expected result', async () => {
+    const id = '1';
+    const result = {
+      id: 1,
+      name: 'John Doe',
+      memories: [
+        {
+          id: 1,
+          title: 'Some title',
+          description: 'Some description',
+          media: [{ id: 1, url: 'http://example.com/image.jpg' }],
+        },
+      ],
+    };
+
+    expect(await controller.findOne(id)).toEqual(result);
+    expect(service.findOne).toHaveBeenCalledWith(1);
   });
 });
