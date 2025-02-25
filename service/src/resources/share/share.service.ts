5193889694b5fc19ade2ce2af96ea530851a1e2e
Omer Mujtaba <omer.mujtaba96@gmail.com>
2025-02-25 01:12:11 -0500

    Add functionality to retrieve memories by user ID in Share module

--- service/src/resources/share/share.service.ts
+++ service/src/resources/share/share.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { MemoriesService } from '../memories/memories.service';
 
 @Injectable()
 export class ShareService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private memoriesService: MemoriesService,
+  ) {}
 
-  async findOne(id: number) {
-    return this.usersService.findOneWithMemories(id);
+  async findOneByUserId(id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  async findAllMemoriesByUserId(
+    id: number,
+    page: number,
+    limit: number,
+    order: string,
+  ) {
+    return this.memoriesService.findAllByUserId(id, page, limit, order);
   }
 }
